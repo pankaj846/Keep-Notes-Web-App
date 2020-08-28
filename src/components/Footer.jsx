9845945f1cf9dbe91621878682bc5c03d6d666ee
Pankaj Singh <56686090+pankaj846@users.noreply.github.com>
2020-08-29 01:20:09 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <a href="https://pankajsingh.me">  <p>Made by Pankaj Singh</p></a>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateIcon from '@material-ui/icons/Create';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CreateIcon />
+        Keep Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
