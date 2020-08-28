5e3426c5fb7586fdaa0580d98d53d3c6587e92cb
Pankaj Singh <56686090+pankaj846@users.noreply.github.com>
2020-08-29 01:18:10 +0530

    Add files via upload

---
+++ build/precache-manifest.b63b4d82f7835050c3a9cc2c71f377ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c16ac2aa7804159e1a229fc8a23acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b3b51e585d1194a24a",
+    "url": "/static/js/2.2e7d5ae2.chunk.js"
+  },
+  {
+    "revision": "54ff34f9b6bb83908414",
+    "url": "/static/js/main.5642d823.chunk.js"
+  },
+  {
+    "revision": "6640bd1ec2e39e109ecf",
+    "url": "/static/js/runtime-main.2aa7a854.js"
+  }
+]);
